962d7d99a43998ee3651da37f2eb856a1f623d9a
MariyaSP <kartsevaSP@yandex.ru>
2022-10-22 22:38:23 +0300

    fixed animate

--- js/modules/burgerMenu.js
+++ js/modules/burgerMenu.js
@@ -1,18 +1,26 @@
 const bMenu = document.querySelector('.burger__menu');
 const constMenu = document.querySelector('.contecst__menu');
+const overlayMenu = document.querySelector('.overlay__menu');
+
 const docEl = document.documentElement;
 let startTime;
-const durationMove = 1500;
+const durationMove = 2500;
 let move;
 
-constMenu.style.cssText = `left: -${constMenu.scrollWidth}px`
+overlayMenu.style.classList = `left: -${constMenu.scrollWidth}px`;
+constMenu.style.cssText = `left: -${constMenu.scrollWidth}px`;
+
 window.addEventListener('resize', () => {constMenu.style.cssText = `left: -${constMenu.scrollWidth}px`;})
 
 bMenu.addEventListener('click', () =>{
     constMenu.classList.toggle('open');
-    startTime = NaN;
-    requestAnimationFrame(menuMove);
-
+    if(constMenu.classList.contains('open')){
+      overlayMenu.style.cssText = `width: 100%`;
+    }
+    else
+      overlayMenu.style.cssText = `width: 0px`;
+      startTime = NaN;
+      requestAnimationFrame(menuMove);
 });
 
 const menuMove = (timestamp) => {
@@ -27,12 +35,22 @@ const menuMove = (timestamp) => {
                 requestAnimationFrame(menuMove);
             }
         } else {
+
             move = constMenu.scrollWidth * (1 - progress);
             if(progress < 1) {
-                constMenu.style.transform = `translateX(${move }px)`;
+                constMenu.style.transform = `translateX(${move}px)`;
                 requestAnimationFrame(menuMove);
             }
-    }  
 
+    }
 }
 
+overlayMenu.addEventListener('click', (e) => {
+    const target = e.target;
+    if(target.closest('.contecst__menu-item') || target === overlayMenu){
+        startTime = NaN;
+        constMenu.classList.remove('open');
+        overlayMenu.style.cssText = `width: 0px`;
+        requestAnimationFrame(menuMove);
+    }
+});
